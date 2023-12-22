4fb3c11985c6648e056ca330c60388521442db3f
abdurezak <abdurazak.awil1@gmail.com>
2023-12-22 18:54:34 +0300

    Remove unused imports from page.tsx

--- src/app/(marketting)/page.tsx
+++ src/app/(marketting)/page.tsx
@@ -1,7 +1,3 @@
-import { Logo } from "@/components/Icons";
-import { siteConfig } from "@/config/site";
-import LogoWithName from "../../components/shared/LogoWithName";
-
 export default function HomePage() {
   return <section></section>;
 }
