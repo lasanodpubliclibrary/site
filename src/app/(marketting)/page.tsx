8d4d3ecffe8e15cd6738dd7c3cc6743709fee109
abdurezak <abdurazak.awil1@gmail.com>
2023-12-22 16:25:39 +0300

    style: make header sticky

--- src/app/(marketting)/page.tsx
+++ src/app/(marketting)/page.tsx
@@ -1,6 +1,6 @@
 import { Logo } from "@/components/Icons";
 import { siteConfig } from "@/config/site";
-import LogoWithName from "../../components/Icons/LogoWithName";
+import LogoWithName from "../../components/shared/LogoWithName";
 
 export default function HomePage() {
   return <section></section>;
