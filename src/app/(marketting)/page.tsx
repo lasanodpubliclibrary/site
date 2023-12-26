d451b26eb235225e996de72e3e8a5be01e1ce190
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2023-12-27 01:46:07 +0300

    Feat: add json-ld to homepage

--- src/app/(marketting)/page.tsx
+++ src/app/(marketting)/page.tsx
@@ -1,3 +1,34 @@
+import { siteConfig } from "@/config/site";
+
 export default function HomePage() {
-  return <section></section>;
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Library",
+    "name": siteConfig.name,
+    "url": siteConfig.url,
+    "logo": `${siteConfig.url}/logo.svg`,
+    "description": siteConfig.description,
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": siteConfig.address.street,
+      "addressLocality": siteConfig.address.city,
+      "addressRegion": siteConfig.address.region,
+      "postalCode": siteConfig.address.postalCode,
+      "addressCountry": siteConfig.address.country,
+    },
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "telephone": siteConfig.contact.telephone,
+      "contactType": "customer service",
+    },
+  };
+
+  return (
+    <section>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+    </section>
+  );
 }
