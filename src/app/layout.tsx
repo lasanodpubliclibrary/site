521cd799fa59441ad429db36ac0c5ef8328c593e
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2023-12-26 15:17:26 +0300

    Update: layout.tsx - change “|” template separator to “-“

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
   title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
+    template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
   openGraph: {
