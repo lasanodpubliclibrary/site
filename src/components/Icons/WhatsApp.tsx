649cb7945989b3fd5e5dd952fde0a50331b16845
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2023-12-27 03:09:28 +0300

    Update WhatsApp.tsx

--- src/components/Icons/WhatsApp.tsx
+++ src/components/Icons/WhatsApp.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const WhatsApp = ({ ...props }: LucideProps) => (
   <svg
-    className="w-8 h-8"
+    className="w-7 h-7"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 52 52"
     fill="white"
     strokeWidth={2}
     {...props}
