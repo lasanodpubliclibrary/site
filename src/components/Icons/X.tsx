e5297610cc219584f35e38b97a6f18ae1b3a61f2
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2023-12-27 03:23:15 +0300

    Update X.tsx

--- src/components/Icons/X.tsx
+++ src/components/Icons/X.tsx
@@ -4,8 +4,6 @@ const X = ({ ...props }: LucideProps) => (
   <svg
     className="w-7 h-7"
     xmlns="http://www.w3.org/2000/svg"
-    width="100"
-    height="100"
     viewBox="0 0 50 50"
     fill="white"
     strokeWidth={2}
