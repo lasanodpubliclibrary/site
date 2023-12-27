9c5cde1b597b3c023c35424e5c2f82873e0c06cb
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2023-12-27 03:03:06 +0300

    Style: X.tsx - increase size

--- src/components/Icons/X.tsx
+++ src/components/Icons/X.tsx
@@ -2,7 +2,7 @@ import { LucideProps } from "lucide-react";
 
 const X = ({ ...props }: LucideProps) => (
   <svg
-    className="w-6 h-6"
+    className="w-7 h-7"
     xmlns="http://www.w3.org/2000/svg"
     width="100"
     height="100"
