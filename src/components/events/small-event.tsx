b006770d920742dcad6521c1d2e2025945e29d5a
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-01-04 22:55:23 +0300

    Update small-event.tsx

--- src/components/events/small-event.tsx
+++ src/components/events/small-event.tsx
@@ -9,19 +9,19 @@ export default function SmallEvent({ title, description, date }: Event) {
       <Image
         src="/logo.svg"
         alt="logo"
-        width={150}
-        height={150}
+        width={120}
+        height={120}
         className="rounded-md"
       />
       <div>
         <h2 className="text-lg md:text-xl font-semibold line-clamp-2 leading-snug tracking-tight">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit.
         </h2>
-        <div className="flex flex-col md:divide-x-2 gap-2 md:flex-row text-muted-foreground">
+        <div className="flex text-sm flex flex-wrap divide-2 gap-2 md:flex-row text-muted-foreground">
           <Moment date={date} format="DD-mm-yyyy" className="pr-2 " />
           <p className="md:px-2 pr-2">The Library hall</p>
         </div>
-        <p className="line-clamp-2 leading-snug">
+        <p className="line-clamp-2 text-sm text-foreground-muted leading-snug">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
           ad laboriosam incidunt, a beatae corrupti odio earum iusto nisi sit
           corporis. Itaque enim reiciendis obcaecati rem accusantium at numquam
