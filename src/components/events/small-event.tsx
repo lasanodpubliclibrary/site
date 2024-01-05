6be54b9c4aaa0c22783ed46fafabfaa1ab810966
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-01-05 09:53:07 +0300

    Update small-event.tsx

--- src/components/events/small-event.tsx
+++ src/components/events/small-event.tsx
@@ -17,11 +17,11 @@ export default function SmallEvent({ title, description, date }: Event) {
         <h2 className="md:text-xl font-semibold line-clamp-2 leading-snug tracking-tight">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit.
         </h2>
-        <div className="flex text-sm flex flex-wrap divide-x-2 gap-2 md:flex-row text-muted-foreground">
+        <div className="flex text-sm flex flex-wrap divide-x-2 gap-x-2  md:flex-row text-muted-foreground">
           <Moment date={date} format="DD-mm-yyyy" className="pr-2 " />
-          <p className="md:px-2 pr-2">The Library hall</p>
+          <p className="px-2 pr-2">The Library hall</p>
         </div>
-        <p className="line-clamp-2 text-sm text-muted-foreground leading-snug">
+        <p className="line-clamp-3 text-sm text-muted-foreground leading-snug">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
           ad laboriosam incidunt, a beatae corrupti odio earum iusto nisi sit
           corporis. Itaque enim reiciendis obcaecati rem accusantium at numquam
